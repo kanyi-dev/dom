bd0119109b4c752513800bf24d6884a96465c004
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-22 22:32:34 +0300

    comply with exercise tassks

--- js-file.js
+++ js-file.js
@@ -12,29 +12,25 @@ const h3 = document .createElement("h3");
 h3.textContent = "I'm a blue h3!";
 h3.style.color = "blue";
 
-// const h3Element = document.createElement("h3");
-// h3Element.textContent = "I’m a blue h3!";
-// h3Element.style.color = "blue";
+// Create a <div> with a black border and pink background color with the following elements inside of it:
 
-// const divElement = document.createElement("div");
-// divElement.style.border = "1px solid black";
-// divElement.style.backgroundColor = "pink";
+// Another <h1> that says "I'm in a div"
 
-// const h1InsideDiv = document.createElement("h1");
-// h1InsideDiv.textContent = "I’m in a div";
 
-// const pInsideDiv = document.createElement("p");
-// pInsideDiv.textContent = "ME TOO!";
+const div = document.createElement("div");
+div.style.border = "1px solid black";
+div.style.backgroundColor = "pink";
 
-// // Append the elements to the container
-//  const container = document.getElementById("container");
-//  container.appendChild(pElement);
-// container.appendChild(h3Element);
-// container.appendChild(divElement);
-// divElement.appendChild(h1InsideDiv);
-// divElement.appendChild(pInsideDiv);
+const h1Child = document.createElement("h1");
+h1Child.textContent = "I'm in a Div";
 
-body.append(p, h3)
+const pChild = document.createElement("p");
+pChild.textContent = "ME TOO!";
+
+
+
+body.append(p, h3, div)
+div.append(h1Child, pChild);
 
 // // Print a success message
-// console.log("Elements added successfully to the container using JavaScript and DOM methods.");
+console.log("Elements added successfully to the container using JavaScript and DOM methods.");
